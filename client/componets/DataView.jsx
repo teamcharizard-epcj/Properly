1d07769d0f369831098f2ff90b26dac46f3fada9
Joao Morgado <jmrmorgado@gmail.com>
2025-08-05 16:16:11 -0400

    WIP(dataView) Working on logic

--- client/componets/DataView.jsx
+++ client/componets/DataView.jsx
@@ -1,20 +1,61 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import 'react-data-grid/lib/styles.css';
 import { TreeDataGrid, DataGrid } from 'react-data-grid';
 
 export default function DataView() {
-  const columns = [
-    { key: 'id', name: 'ID' },
-    { key: 'title', name: 'Title' },
-  ];
+  const [data, setData] = useState([]);
+  const [columns, setColumns] = useState([]);
+  const [rows, setRows] = useState([]);
 
-  const rows = [
-    { id: 0, title: 'Example' },
-    { id: 1, title: 'Demo' },
-  ];
+  // This will eventually be a fetch
+  useEffect(() => {
+    setData({
+      properties: [
+        {
+          id: 0,
+          name: 'Beach House',
+          tasks: [
+            { id: 0, name: 'Replace Fridge Light', status: 'Pending' },
+            { id: 1, name: 'Empty Trash', status: 'Pending' },
+          ],
+        },
+        {
+          id: 1,
+          name: 'Mountain Cabin',
+          tasks: [
+            { id: 0, name: 'Replace Air Filter', status: 'Pending' },
+            { id: 1, name: 'Close Crawlspace Vents', status: 'Pending' },
+          ],
+        },
+      ],
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!data.properties) {
+      return;
+    }
+
+    function getProperties() {
+      const result = [];
+
+      data.properties.forEach((j) => {
+        result.push({ properties: j.name, tasks: j.id });
+      });
+      return result;
+    }
+
+    const myProperties = getProperties();
+
+    setColumns([
+      { key: 'properties', name: 'Properties' },
+      { key: 'tasks', name: 'Address' },
+    ]);
+    setRows(myProperties);
+  }, [data]);
 
   return <DataGrid columns={columns} rows={rows} />;
 }
