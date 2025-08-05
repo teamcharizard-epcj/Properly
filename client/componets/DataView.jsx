f419e16c5a0b715f3238d8f8032bdf04a25d586a
Joao Morgado <jmrmorgado@gmail.com>
2025-08-05 19:19:18 -0400

    feat(table) new HTML based table

--- client/componets/DataView.jsx
+++ client/componets/DataView.jsx
@@ -2,60 +2,52 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import 'react-data-grid/lib/styles.css';
-import { TreeDataGrid, DataGrid } from 'react-data-grid';
 
 export default function DataView() {
-  const [data, setData] = useState([]);
-  const [columns, setColumns] = useState([]);
-  const [rows, setRows] = useState([]);
+  const data = {
+    properties: [
+      {
+        id: 0,
+        name: 'Beach House',
+        address: 'Sunset dr 10',
+        tasks: [
+          { id: 0, name: 'Replace Fridge Light', status: 'Pending' },
+          { id: 1, name: 'Empty Trash', status: 'Pending' },
+        ],
+      },
+      {
+        id: 1,
+        name: 'Mountain Cabin',
+        address: 'Peak road 120',
+        tasks: [
+          { id: 0, name: 'Replace Air Filter', status: 'Pending' },
+          { id: 1, name: 'Close Crawlspace Vents', status: 'Pending' },
+          { id: 1, name: 'Pest Prevention', status: 'Pending' },
+        ],
+      },
+    ],
+  };
 
-  // This will eventually be a fetch
-  useEffect(() => {
-    setData({
-      properties: [
-        {
-          id: 0,
-          name: 'Beach House',
-          tasks: [
-            { id: 0, name: 'Replace Fridge Light', status: 'Pending' },
-            { id: 1, name: 'Empty Trash', status: 'Pending' },
-          ],
-        },
-        {
-          id: 1,
-          name: 'Mountain Cabin',
-          tasks: [
-            { id: 0, name: 'Replace Air Filter', status: 'Pending' },
-            { id: 1, name: 'Close Crawlspace Vents', status: 'Pending' },
-          ],
-        },
-      ],
-    });
-  }, []);
-
-  useEffect(() => {
-    if (!data.properties) {
-      return;
-    }
-
-    function getProperties() {
-      const result = [];
-
-      data.properties.forEach((j) => {
-        result.push({ properties: j.name, tasks: j.id });
-      });
-      return result;
-    }
-
-    const myProperties = getProperties();
-
-    setColumns([
-      { key: 'properties', name: 'Properties' },
-      { key: 'tasks', name: 'Address' },
-    ]);
-    setRows(myProperties);
-  }, [data]);
-
-  return <DataGrid columns={columns} rows={rows} />;
+  return (
+    <div>
+      <table className="dataView">
+        <thead>
+          <tr>
+            <th>Property</th>
+            <th>Address</th>
+            <th>Tasks</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.properties.map((e) => {
+            return(<tr>
+              <td>{e.name}</td>
+              <td>{e.address}</td>
+              <td>{e.tasks.length}</td>
+            </tr>)
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
